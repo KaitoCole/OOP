2bdf9068909cffafe217fb4aa8d0d74bd6fd9ff8
KaitoCole <165266999+KaitoCole@users.noreply.github.com>
2024-10-10 14:29:50 +0800

    Problem3.php

--- Problem3.php
+++ Problem3.php
@@ -9,7 +9,19 @@ class Vehicle {
         $this->speed = $speed;
         $this->mileage = $mileage;
     }
+    public function get_name () { 
+        return $this->name;
+    }
+    public function get_speed () {
+        return $this->speed;
+    }
+    public function get_mileage () {
+        return $this->mileage;
+    }
 }
 class Bus extends Vehicle{
 }
+
+$bus = new Bus("Victory",50,60);
+echo "The bus name is " . $bus->get_name(). " and its speed is " . $bus->get_speed() . " and its mileage is " . $bus->get_mileage ();
 ?>
